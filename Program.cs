54a55e54497b0037bd92013b519e2947e0aebdab
Tremblay, Joe <joe.tremblay@ncr.com>
2019-11-26 16:44:49 -0600

    initial commit: discriminated union example

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace language_ext_playground
+{
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         discriminated_union.ShapeExample.Run()
+            .Iter(Log);
+      }
+
+
+      static void Log(string message) =>
+         Console.WriteLine(message);
+   }
+}
---
+++ discriminated-union/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LanguageExt;
+using System.Linq;
+using static LanguageExt.Prelude;
+
+namespace language_ext_playground.discriminated_union
+{
+   [Union]
+   public interface Shape
+   {
+      Shape Rectangle(float width, float length);
+      Shape Circle(float radius);
+      Shape Prism(float width, float height);
+   }
+
+   public static class ShapeExample
+   {
+      public static Lst<string> Run()
+      {
+         var s1 = ShapeCon.Rectangle(100, 20);
+         var s2 = ShapeCon.Circle(50);
+         var s3 = ShapeCon.Prism(25, 300);
+
+         return List(s1, s2, s3)
+            .Map(GetDescription);
+      }
+
+      static string GetDescription(Shape s) =>
+         s switch
+         {
+            Rectangle rectangle => $"rectangle: w={rectangle.Width} l={rectangle.Length}",
+            Circle circle => $"circle: r={circle.Radius}",
+            Prism prism => $"prism: w={prism.Width} h={prism.Height}",
+            _ => "unknown shape"
+         };
+   }
+}
