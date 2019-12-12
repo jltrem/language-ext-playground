7993b3b01a521eddd0866d81aace5dd6c859d5b4
Tremblay, Joe <joe.tremblay@ncr.com>
2019-12-12 10:48:49 -0600

    Either example updates

--- either/OneThingOrAnother.cs
+++ either/OneThingOrAnother.cs
@@ -8,13 +8,18 @@ namespace language_ext_playground.either
     public static class OneThingOrAnother
     {
         public static Either<string, int> TryCheck42(int value) =>
-            Try<Either<string, int>>(() =>
+            Try(() =>
 
                 value == 42
                     ? 42
                     : throw new Exception("the value was not 42")
             )
-            .IfFail(ex => ex.Message);
+            .ToEither(ex => ex.Message);
+
+        public static Either<string, int> PreludeCheck42(int value) =>
+            value == 42
+                ? Right<string, int>(42)
+                : Left<string, int>("the value was not 42");
 
         public static Either<string, int> Check42(int value)
         {
